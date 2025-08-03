873c891d56bafdb82407aacc71a8836480328238
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-03 16:02:47 -0300

    feat(menu): add new navigation items

---
+++ src/lib/constants.tsx
@@ -0,0 +1,7 @@
+export const menuItems = [
+  { title: "Início", path: "/" },
+  { title: "Serviços", path: "/servicos" },
+  { title: "Sobre", path: "/sobre" },
+  { title: "Depoimentos", path: "/depoimentos" },
+  { title: "Contato", path: "/contato" },
+];
