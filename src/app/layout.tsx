600afa13e517f60eb09f399d4a677d66f0b6d909
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-03 16:07:48 -0300

    feat(layout): add Header component and update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Infralink Brasil",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
